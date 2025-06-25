39103be82df6a5a04f11f3fe68b32637bf1b7e6c
Jan-Simon Winkelmann <jwinkelmann@rapidmail.de>
2025-06-25 10:15:10 +0200

    Add more broken stuff

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
-// Verbindung zur Datenbank (ohne Fehlerbehandlung)
 $conn = mysqli_connect("localhost", "root", "", "test");
 
-// User-Eingabe direkt aus GET-Parametern
 $username = $_GET['user'];
 $password = $_GET['pass'];
 
-// SQL-Injection möglich
 $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
 $result = mysqli_query($conn, $query);
 
-// Ausgabe des Ergebnisses (kann sensitive Daten enthalten)
 if ($row = mysqli_fetch_assoc($result)) {
     echo "Willkommen, " . $row['username'] . "!<br>";
     echo "Dein Passwort ist: " . $row['password'];
@@ -18,14 +14,29 @@ if ($row = mysqli_fetch_assoc($result)) {
     echo "Login fehlgeschlagen.";
 }
 
-// Datei-Upload ohne Prüfung
+try {
+    json_decode(json: 'no', flags: JSON_THROW_ON_ERROR);
+} catch (JsonException $e) {
+    echo "Ein Fehler ist aufgetreten: " . $e->getMessage();
+}
+
 if (isset($_FILES['upload'])) {
     move_uploaded_file($_FILES['upload']['tmp_name'], "uploads/" . $_FILES['upload']['name']);
     echo "Datei hochgeladen!";
 }
 
-// XSS durch Rückgabe ungefilterter Eingaben
 if (isset($_GET['msg'])) {
     echo "Nachricht: " . $_GET['msg'];
 }
+
+$res = mysqli_fetch_assoc(mysqli_query("SELECT html FROM mailing WHERE mailing_id=1"));
+echo $res['html'];
+
+echo file_get_contents($_GET['file']);
+
 ?>
+
+<script
+    src="https://code.jquery.com/jquery-1.12.4.js"
+    integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+    crossorigin="anonymous"></script>
